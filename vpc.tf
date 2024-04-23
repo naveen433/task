2b577cc4d7875aac725e7457316c495dbf910314
Naveen <ke.naveen433@gmail.com>
2024-04-23 17:49:52 +0530

    create public and private subnets

---
+++ vpc.tf
@@ -0,0 +1,43 @@
+module "label_vpc" {
+  source     = "cloudposse/label/null"
+  version    = "0.25.0"
+  context    = module.base_label.context
+  name       = "vpc"
+  attributes = ["main"]
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags                 = module.label_vpc.tags
+}
+
+# =========================
+# Create your subnets here
+# =========================
+data "aws_availability_zones" "available" {}resource "aws_vpc" "main" {
+  cidr_block           = "192.170.0.0/20"
+  enable_dns_hostnames = true
+  tags {
+    Name = "main"
+  }
+}resource "aws_subnet" "public_subnet" {
+  count = "${length(data.aws_availability_zones.available.names)}"
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "192.170.${10+count.index}.0/24"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  map_public_ip_on_launch = true
+  tags {
+    Name = "PublicSubnet"
+  }
+}resource "aws_subnet" "private_subnet" {
+  count = "${length(data.aws_availability_zones.available.names)}"
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "192.170.${20+count.index}.0/24"
+  availability_zone= "${data.aws_availability_zones.available.names[count.index]}"
+  map_public_ip_on_launch = false
+  tags {
+    Name = "PrivateSubnet"
+  }
+}
