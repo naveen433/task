762c52720070415d9235c43d8db15abc7cb74155
Naveen <ke.naveen433@gmail.com>
2024-04-30 13:50:35 +0530

    create resources

--- vpc.tf
+++ vpc.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "public_subnet" {
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   map_public_ip_on_launch = true
   tags = {
-    Name = "PublicSubnet"
+    Name = "Public Subnet ${count.index + 1}"
   }
 }
 resource "aws_subnet" "private_subnet" {
@@ -33,6 +33,124 @@ resource "aws_subnet" "private_subnet" {
   availability_zone= "${data.aws_availability_zones.available.names[count.index]}"
   map_public_ip_on_launch = false
   tags = {
-    Name = "PrivateSubnet"
+    Name = "Private Subnet ${count.index + 1}"
   }
 }
+
+# Creating an Internet Gateway for the VPC
+resource "aws_internet_gateway" "Internet_Gateway" {
+  depends_on = [
+    aws_vpc.main,
+    aws_subnet.public_subnet,
+    aws_subnet.private_subnet
+  ]
+  
+  # VPC in which it has to be created!
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "IG-Public-&-Private-VPC"
+  }
+}
+
+# Creating an Route Table for the public subnet!
+resource "aws_route_table" "Public-Subnet-RT" {
+  depends_on = [
+    aws_vpc.main,
+    aws_internet_gateway.Internet_Gateway
+  ]
+
+   # VPC ID
+  vpc_id = "${aws_vpc.main.id}"
+
+  # NAT Rule
+  route {
+    cidr_block = element(var.public_subnet_cidr, count.index)
+    gateway_id = aws_internet_gateway.Internet_Gateway.id
+  }
+
+  tags = {
+    Name = "Route Table for Internet Gateway"
+  }
+}
+
+# Creating a resource for the Route Table Association!
+resource "aws_route_table_association" "RT-IG-Association" {
+
+  depends_on = [
+    aws_vpc.main,
+    aws_subnet.public_subnet,
+    aws_subnet.private_subnet,
+    aws_route_table.Public-Subnet-RT
+  ]
+
+# Public Subnet ID
+  subnet_id      = aws_subnet.public_subnet.id
+
+#  Route Table ID
+  route_table_id = aws_route_table.Public-Subnet-RT.id
+}
+
+# Creating an Elastic IP for the NAT Gateway!
+resource "aws_eip" "Nat-Gateway-EIP" {
+  depends_on = [
+    aws_route_table_association.RT-IG-Association
+  ]
+  
+}
+
+# Creating a NAT Gateway!
+resource "aws_nat_gateway" "NAT_GATEWAY" {
+  depends_on = [
+    aws_eip.Nat-Gateway-EIP
+  ]
+
+  # Allocating the Elastic IP to the NAT Gateway!
+  allocation_id = aws_eip.Nat-Gateway-EIP.id
+  
+  # Associating it in the Public Subnet!
+  subnet_id = aws_subnet.public_subnet.id
+  tags = {
+    Name = "Nat-Gateway_Project"
+  }
+}
+
+# Creating a Route Table for the Nat Gateway!
+resource "aws_route_table" "NAT-Gateway-RT" {
+  depends_on = [
+    aws_nat_gateway.NAT_GATEWAY
+  ]
+
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = element(var.private_subnet_cidr, count.index)
+    nat_gateway_id = aws_nat_gateway.NAT_GATEWAY.id
+  }
+
+  tags = {
+    Name = "Route Table for NAT Gateway"
+  }
+
+}
+
+# Creating an Route Table Association of the NAT Gateway route 
+# table with the Private Subnet!
+resource "aws_route_table_association" "Nat-Gateway-RT-Association" {
+  depends_on = [
+    aws_route_table.NAT-Gateway-RT
+  ]
+
+#  Private Subnet ID for adding this route table to the DHCP server of Private subnet!
+  subnet_id      = aws_subnet.private_subnet.id
+
+# Route Table ID
+  route_table_id = aws_route_table.NAT-Gateway-RT.id
+}
+
+
+
+
+
+
+
