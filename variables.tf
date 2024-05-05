404b69745440414b137eb01a76e9a60e02bd312c
Naveen <ke.naveen433@gmail.com>
2024-05-05 16:34:38 +0530

    creating resources like igw natgw route tables

--- variables.tf
+++ variables.tf
@@ -7,6 +7,7 @@ variable "aws_region" {
 variable "aws_access_key" {
   description = "AWS Access Key ID for the target AWS account"
   type        = string
+ 
 }
 
 variable "aws_secret_key" {
@@ -27,25 +28,5 @@ variable "vpc_cidr" {
   validation {
     condition     = tonumber(split("/", var.vpc_cidr)[1]) <= 20 && tonumber(split("/", var.vpc_cidr)[1]) >= 16
     error_message = "CIDR size must be at least /20 and no larger than /16"
-  }
-}
-
-variable "public_subnet_cidr" {
-  type = list(string)
-  description = "The IPv4 CIDR block for the public subnet"
-  default     = ["192.170.1.0/24", "192.170.2.0/24", "192.170.3.0/24"]
-  validation {
-    condition     = tonumber(split("/", var.public_subnet_cidr)[1]) <= 28 && tonumber(split("/", var.public_subnet_cidr)[1]) >= 24
-    error_message = "CIDR size must be at least /28 and no lorger than 24"
-  }
-}
-
-variable "private_subnet_cidr" {
-  type = list(string)
-  description = "The IPv4 CIDR block for the private subnet"
-  default     = ["192.170.1.0/24", "192.170.2.0/24", "192.170.3.0/24"]
-   validation {
-    condition     = tonumber(split("/", var.private_subnet_cidr)[1]) <= 28 && tonumber(split("/", var.private_subnet_cidr)[1]) >= 24
-    error_message = "CIDR size must be at least /28 and no lorger than 24"
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,13 @@
 module "label_vpc" {
-  source     = "cloudposse/label/null"
-  version    = "0.25.0"
-  namespace  = "label"
-  name       = "vpc"
-  delimiter  = "_"
+  source    = "cloudposse/label/null"
+  version   = "0.25.0"
+  namespace = "label"
+  name      = "vpc"
+  delimiter = "_"
 
   tags = {
     Name = "vpc"
-  } 
+  }
 }
 
 resource "aws_vpc" "main" {
@@ -16,141 +16,120 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
   tags                 = module.label_vpc.tags
 }
-
 # =========================
 # Create your subnets here
 # =========================
-resource "aws_subnet" "public_subnet" {
-  count = "${length(data.aws_availability_zones.available.names)}"
-  vpc_id = "${aws_vpc.main.id}"
-  cidr_block = element(var.public_subnet_cidr, count.index)
-  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
-  map_public_ip_on_launch = true
-  tags = module.label_vpc.tags
-}
-resource "aws_subnet" "private_subnet" {
-  count = "${length(data.aws_availability_zones.available.names)}"
-  vpc_id = "${aws_vpc.main.id}"
-  cidr_block = element(var.private_subnet_cidr, count.index)
-  availability_zone= "${data.aws_availability_zones.available.names[count.index]}"
-  map_public_ip_on_launch = false
-  tags = module.label_vpc.tags
+
+module "subnet_addresses" {
+  source = "hashicorp/subnets/cidr"
+
+  base_cidr_block = var.vpc_cidr
+  networks = [
+    {
+      name     = "public"
+      new_bits = local.subnet_cidr_new_bits
+    },
+    {
+      name     = "private"
+      new_bits = local.subnet_cidr_new_bits
+    }
+  ]
 }
 
-# Creating an Internet Gateway for the VPC
-resource "aws_internet_gateway" "Internet_Gateway" {
-  depends_on = [
-    aws_vpc.main,
-    aws_subnet.public_subnet,
-    aws_subnet.private_subnet
-  ]
-  
-  # VPC in which it has to be created!
-  vpc_id = "${aws_vpc.main.id}"
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "label_public_subnet" {
+  source    = "cloudposse/label/null"
+  version   = "0.25.0"
+  namespace = "label"
+  name      = "public-subnet"
+  delimiter = "_"
 
   tags = {
+    Name = "public-subnet"
+  }
+}
+
+module "label_private_subnet" {
+  source    = "cloudposse/label/null"
+  version   = "0.25.0"
+  namespace = "label"
+  name      = "private-subnet"
+  delimiter = "_"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+locals {
+  cidr_parts           = split("/", var.vpc_cidr)
+  network_bits         = element(local.cidr_parts, 3)
+  subnet_prefix        = 24
+  subnet_cidr_new_bits = local.subnet_prefix - local.network_bits
+}
+
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.id
+     tags = {
     Name = "IG-Public-&-Private-VPC"
   }
 }
 
-# Creating an Route Table for the public subnet!
-resource "aws_route_table" "Public-Subnet-RT" {
-  depends_on = [
-    aws_vpc.main,
-    aws_internet_gateway.Internet_Gateway
-  ]
-
-   # VPC ID
-  vpc_id = "${aws_vpc.main.id}"
-
-  # NAT Rule
-  route {
-    cidr_block = element(var.public_subnet_cidr, count.index)
-    gateway_id = aws_internet_gateway.Internet_Gateway.id
-  }
-
-  tags = {
-    Name = "Route Table for Internet Gateway"
-  }
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.main.id
+  subnet_id     = aws_subnet.public_subnet.id
+  tags = module.label_vpc
+  depends_on = [aws_internet_gateway.main]
 }
 
-# Creating a resource for the Route Table Association!
-resource "aws_route_table_association" "RT-IG-Association" {
-
-  depends_on = [
-    aws_vpc.main,
-    aws_subnet.public_subnet,
-    aws_subnet.private_subnet,
-    aws_route_table.Public-Subnet-RT
-  ]
-
-# Public Subnet ID
-  subnet_id      = aws_subnet.public_subnet.id
-
-#  Route Table ID
-  route_table_id = aws_route_table.Public-Subnet-RT.id
-}
-
-# Creating an Elastic IP for the NAT Gateway!
-resource "aws_eip" "Nat-Gateway-EIP" {
-  depends_on = [
-    aws_route_table_association.RT-IG-Association
-  ]
+resource "aws_eip" "main" {
+    domain = "vpc"
+    depends_on = [ aws_internet_gateway.main ]
   
 }
 
-# Creating a NAT Gateway!
-resource "aws_nat_gateway" "NAT_GATEWAY" {
-  depends_on = [
-    aws_eip.Nat-Gateway-EIP
-  ]
-
-  # Allocating the Elastic IP to the NAT Gateway!
-  allocation_id = aws_eip.Nat-Gateway-EIP.id
-  
-  # Associating it in the Public Subnet!
-  subnet_id = aws_subnet.public_subnet.id
-  tags = {
-    Name = "Nat-Gateway_Project"
-  }
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = data.aws_availability_zones.available.names[0]
+  cidr_block        = module.subnet_addresses.network_cidr_blocks.public
+  tags              = module.label_public_subnet.tags
 }
 
-# Creating a Route Table for the Nat Gateway!
-resource "aws_route_table" "NAT-Gateway-RT" {
-  depends_on = [
-    aws_nat_gateway.NAT_GATEWAY
-  ]
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = data.aws_availability_zones.available.names[1]
+  cidr_block        = module.subnet_addresses.network_cidr_blocks.private
+  tags              = module.label_private_subnet.tags
+}
 
-  vpc_id = "${aws_vpc.main.id}"
-
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
   route {
-    cidr_block = element(var.private_subnet_cidr, count.index)
-    nat_gateway_id = aws_nat_gateway.NAT_GATEWAY.id
-  }
-
-  tags = {
-    Name = "Route Table for NAT Gateway"
-  }
-
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+  tags = module.label_private_subnet.tags
+}
+resource "aws_route_table_association" "public" {
+    subnet_id = aws_subnet.public_subnet.id
+    route_table_id = aws_route_table.public.id
 }
 
-# Creating an Route Table Association of the NAT Gateway route 
-# table with the Private Subnet!
-resource "aws_route_table_association" "Nat-Gateway-RT-Association" {
-  depends_on = [
-    aws_route_table.NAT-Gateway-RT
-  ]
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.main.id
+  }
 
-#  Private Subnet ID for adding this route table to the DHCP server of Private subnet!
-  subnet_id      = aws_subnet.private_subnet.id
-
-# Route Table ID
-  route_table_id = aws_route_table.NAT-Gateway-RT.id
+  tags = module.label_private_subnet.tags
 }
 
-
-
-
-
-
-
+resource "aws_route_table_association" "private" {
+    subnet_id = aws_subnet.private_subnet.id
+    route_table_id = aws_route_table.private.id
+}
